e3c480b394fdc3e029891661b1352f3682187388
AmitGZ <amitzohar4@gmail.com>
2023-05-03 22:44:34 +0300

    added images and login

--- app/src/main/java/com/android/example/shiftmanagementapp/ShiftActivity.java
+++ app/src/main/java/com/android/example/shiftmanagementapp/ShiftActivity.java
@@ -1,9 +1,14 @@
 package com.android.example.shiftmanagementapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class ShiftActivity extends AppCompatActivity
 {
@@ -16,6 +21,35 @@ public class ShiftActivity extends AppCompatActivity
     
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     
+        ImageButton myImageButton = findViewById(R.id.imageButton);
+        myImageButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                System.out.println("Click");
+            }
+        });
+    
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.home_item:
+                        // Handle click for home item
+                        return true;
+                    case R.id.search_item:
+                        // Handle click for search item
+                        return true;
+                    case R.id.profile_item:
+                        // Handle click for profile item
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+        });
     }
     
     @Override
